2cc2000e88a73f7add7f5214cb712290e16f1e08
Caedis <Caedis@users.noreply.github.com>
2025-06-01 21:11:45 -0500

    Add spectator to tab completion

--- src/main/java/ganymedes01/etfuturum/mixins/early/spectator/MixinCommandGameMode.java
+++ src/main/java/ganymedes01/etfuturum/mixins/early/spectator/MixinCommandGameMode.java
@@ -7,7 +7,11 @@ import net.minecraft.world.WorldSettings;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 @Mixin(CommandGameMode.class)
 public class MixinCommandGameMode {
@@ -16,5 +20,17 @@ public class MixinCommandGameMode {
         if (arg.equalsIgnoreCase("sp") || arg.equalsIgnoreCase("spectator")) {
             cir.setReturnValue(SpectatorMode.SPECTATOR_GAMETYPE);
         }
+    }
+
+    @ModifyArg(method = "addTabCompletionOptions", at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/command/CommandGameMode;getListOfStringsMatchingLastWord([Ljava/lang/String;[Ljava/lang/String;)Ljava/util/List;",
+            ordinal = 0), index = 1)
+    private String[] addSpectatorToTabCompletion(String[] par1) {
+        var ret = new String[par1.length + 1];
+        System.arraycopy(par1, 0, ret, 0, par1.length);
+        ret[par1.length] = "spectator";
+
+        return ret;
     }
 }
