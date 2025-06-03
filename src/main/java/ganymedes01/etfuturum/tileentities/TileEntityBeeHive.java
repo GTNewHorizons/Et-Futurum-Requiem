4e4cbde674cd3055e410720ac19ae59f1dbe2ddb
Maya <10861407+serenibyss@users.noreply.github.com>
2025-06-02 22:54:16 -0600

    Merge branch 'pr/598' into dev
    (cherry picked from commit 9a6869df1059999ecc5552c6c57bc9d0954590bb)

--- src/main/java/ganymedes01/etfuturum/tileentities/TileEntityBeeHive.java
+++ src/main/java/ganymedes01/etfuturum/tileentities/TileEntityBeeHive.java
@@ -1,7 +1,6 @@
 package ganymedes01.etfuturum.tileentities;
 
 import com.google.common.collect.Lists;
-import com.google.common.collect.Maps;
 import com.google.common.collect.Sets;
 import ganymedes01.etfuturum.api.mappings.RegistryMapping;
 import ganymedes01.etfuturum.blocks.BlockBeeHive;
@@ -24,7 +23,6 @@ import roadhog360.hogutils.api.hogtags.helpers.BlockTags;
 
 import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 
 public class TileEntityBeeHive extends TileEntity {
