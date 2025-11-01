eb1d6f6142ae9f6f01cf46e694eae84cdabc5654
Worive <13164341+Worive@users.noreply.github.com>
2025-11-01 12:14:30 +0100

    Update gtnh with upstream changes (#18)
    Co-authored-by: roadhog360 <roadhog360@outlook.com>
    Co-authored-by: Caedis <Caedis@users.noreply.github.com>
    Co-authored-by: embeddedt <42941056+embeddedt@users.noreply.github.com>
    Co-authored-by: brandyn <sonicbrandyn@gmail.com>
    Co-authored-by: Robert <11903511+Roadhog360@users.noreply.github.com>
    Co-authored-by: Maya <10861407+serenibyss@users.noreply.github.com>
    Co-authored-by: FourIsTheNumber <33456283+fouristhenumber@users.noreply.github.com>
    Co-authored-by: Mika Hensel <41554385+MalTeeez@users.noreply.github.com>
    Co-authored-by: Alexdoru <57050655+Alexdoru@users.noreply.github.com>
    Co-authored-by: GamingB3ast <dennis.ortin@gmail.com>
    Co-authored-by: koolkrafter5 <creepersrgreen@gmail.com>

--- src/main/java/ganymedes01/etfuturum/mixinplugin/EtFuturumLateMixins.java
+++ src/main/java/ganymedes01/etfuturum/mixinplugin/EtFuturumLateMixins.java
@@ -3,9 +3,11 @@ package ganymedes01.etfuturum.mixinplugin;
 import com.gtnewhorizon.gtnhmixins.ILateMixinLoader;
 import com.gtnewhorizon.gtnhmixins.LateMixin;
 import ganymedes01.etfuturum.Tags;
+import ganymedes01.etfuturum.configuration.configs.ConfigBlocksItems;
 import ganymedes01.etfuturum.configuration.configs.ConfigMixins;
+import ganymedes01.etfuturum.configuration.configs.ConfigModCompat;
+import it.unimi.dsi.fastutil.objects.ObjectArrayList;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
@@ -18,7 +20,11 @@ public class EtFuturumLateMixins implements ILateMixinLoader {
 
 	@Override
 	public List<String> getMixins(Set<String> loadedMods) {
-		List<String> mixins = new ArrayList<>();
+		List<String> mixins = new ObjectArrayList<>();
+
+		if (ConfigMixins.enableElytra && loadedMods.stream().anyMatch(name -> name.equals("Thaumcraft"))) {
+			mixins.add("backlytra.thaumcraft.MixinEventHandlerEntity");
+		}
 
 		if (ConfigMixins.enableSpectatorMode) {
 			if (loadedMods.contains("IronChest")) {
@@ -27,6 +33,10 @@ public class EtFuturumLateMixins implements ILateMixinLoader {
 			if (loadedMods.contains("appliedenergistics2")) {
 				mixins.add("spectator.MixinPacketInventoryAction");
 			}
+		}
+
+		if(ConfigBlocksItems.enableDeepslateOres && ConfigModCompat.moddedDeepslateOres && !ConfigModCompat.moddedDeepslateOresBlacklist.contains("appliedenergistics2") && loadedMods.contains("appliedenergistics2")) {
+			mixins.add("deepslateores.MixinRenderQuartzOre");
 		}
 
 		return mixins;
