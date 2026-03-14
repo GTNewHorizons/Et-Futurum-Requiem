08cce77310290cefc1dd37520a2ff6b47c8782c0
Caedis <Caedis@users.noreply.github.com>
2026-03-14 01:12:25 -0500

    Rework bubble columns
    Single block id, meta 0 = up, 1 = down
    Remove block texture with particles
    Entities get air in a bubble column
    Fix rendering
    Up columns reset fall distance
    Added sounds

---
+++ src/main/java/ganymedes01/etfuturum/client/particle/BubbleColumnFX.java
@@ -0,0 +1,36 @@
+package ganymedes01.etfuturum.client.particle;
+
+import net.minecraft.block.material.Material;
+import net.minecraft.client.particle.EntityBubbleFX;
+import net.minecraft.util.MathHelper;
+import net.minecraft.world.World;
+
+public class BubbleColumnFX extends EntityBubbleFX {
+
+	public BubbleColumnFX(World world, double x, double y, double z, double xSpeed, double ySpeed, double zSpeed) {
+		super(world, x, y, z, xSpeed, ySpeed, zSpeed);
+		// lasts longer than a normal bubble
+		this.particleMaxAge = (int) (40.0D / (this.worldObj.rand.nextDouble() * 0.8D + 0.2D));
+	}
+
+	@Override
+	public void onUpdate() {
+		this.prevPosX = this.posX;
+		this.prevPosY = this.posY;
+		this.prevPosZ = this.posZ;
+		this.motionY += 0.005D;
+		this.moveEntity(this.motionX, this.motionY, this.motionZ);
+		// friction = 0.85
+		this.motionX *= 0.85D;
+		this.motionY *= 0.85D;
+		this.motionZ *= 0.85D;
+
+		if (this.worldObj.getBlock(MathHelper.floor_double(this.posX), MathHelper.floor_double(this.posY), MathHelper.floor_double(this.posZ)).getMaterial() != Material.water) {
+			this.setDead();
+		}
+
+		if (this.particleMaxAge-- <= 0) {
+			this.setDead();
+		}
+	}
+}
