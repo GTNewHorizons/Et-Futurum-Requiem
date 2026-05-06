c8595cb0bdb52f55cee43be25e99010511856f40
Andrew <me@dragovorn.com>
2026-05-06 00:22:07 -0400

    allow 1.8 skin renderer to support delegated model bipeds to improve compatibility

--- src/main/java/ganymedes01/etfuturum/client/model/ModelPlayer.java
+++ src/main/java/ganymedes01/etfuturum/client/model/ModelPlayer.java
@@ -1,8 +1,14 @@
 package ganymedes01.etfuturum.client.model;
 
+import ganymedes01.etfuturum.client.skins.DelegatedModelBiped;
+import ganymedes01.etfuturum.client.skins.PlayerModelManager;
 import net.minecraft.client.model.ModelBiped;
 import net.minecraft.client.model.ModelRenderer;
 import net.minecraft.entity.Entity;
+import org.lwjgl.opengl.GL11;
+
+import java.util.HashSet;
+import java.util.Set;
 
 public class ModelPlayer extends ModelBiped {
 
@@ -18,6 +24,8 @@ public class ModelPlayer extends ModelBiped {
 	public ModelRenderer bipedRightLegwear;
 	public ModelRenderer bipedBodyWear;
 	public boolean smallArms;
+
+	private Set<DelegatedModelBiped> delegates = new HashSet<>();
 
 	public ModelPlayer(float z, boolean smallArms) {
 		super(z, 0.0F, 64, 64);
@@ -75,11 +83,70 @@ public class ModelPlayer extends ModelBiped {
 		bipedBody.addChild(bipedBodyWear);
 	}
 
+	public void regenerateDelegates() {
+		this.delegates = PlayerModelManager.constructDelegatesFor(this);
+	}
+
+	public int getDelegatesCount() {
+		return delegates.size();
+	}
+
 	@Override
 	public void render(Entity p_render_1_, float p_render_2_, float p_render_3_, float p_render_4_, float p_render_5_, float p_render_6_, float p_render_7_) {
-		super.render(p_render_1_, p_render_2_, p_render_3_, p_render_4_, p_render_5_, p_render_6_, p_render_7_);
+		this.setRotationAngles(p_render_2_, p_render_3_, p_render_4_, p_render_5_, p_render_6_, p_render_7_, p_render_1_);
+		this.delegates.forEach(delegate -> delegate.preRender(p_render_1_, p_render_2_, p_render_3_, p_render_4_, p_render_5_, p_render_6_, p_render_7_));
 
-		if (smallArms)
-			bipedRightArm.rotationPointX += 1.0F;
+		// code stolen from original ModelBiped so that delegation can occur AFTER setRotationAngles but before body rendering
+		// alternative implementation is to add preRender delegate after setRotationAngles, but in the event that it's used elsewhere for some reason
+		// this approach is more "compatible"
+		if (this.isChild) {
+			float f6 = 2.0F;
+			GL11.glPushMatrix();
+			GL11.glScalef(1.5F / f6, 1.5F / f6, 1.5F / f6);
+			GL11.glTranslatef(0.0F, 16.0F * p_render_7_, 0.0F);
+			this.bipedHead.render(p_render_7_);
+			GL11.glPopMatrix();
+			GL11.glPushMatrix();
+			GL11.glScalef(1.0F / f6, 1.0F / f6, 1.0F / f6);
+			GL11.glTranslatef(0.0F, 24.0F * p_render_7_, 0.0F);
+			this.bipedBody.render(p_render_7_);
+			this.bipedRightArm.render(p_render_7_);
+			this.bipedLeftArm.render(p_render_7_);
+			this.bipedRightLeg.render(p_render_7_);
+			this.bipedLeftLeg.render(p_render_7_);
+			this.bipedHeadwear.render(p_render_7_);
+			GL11.glPopMatrix();
+		} else {
+			this.bipedHead.render(p_render_7_);
+			this.bipedBody.render(p_render_7_);
+			this.bipedRightArm.render(p_render_7_);
+			this.bipedLeftArm.render(p_render_7_);
+			this.bipedRightLeg.render(p_render_7_);
+			this.bipedLeftLeg.render(p_render_7_);
+			this.bipedHeadwear.render(p_render_7_);
+		}
+		// end stolen code
+		this.delegates.forEach(delegate -> delegate.postRender(p_render_1_, p_render_2_, p_render_3_, p_render_4_, p_render_5_, p_render_6_, p_render_7_));
+	}
+
+	@Override
+	public void setRotationAngles(float p_78087_1_, float p_78087_2_, float p_78087_3_, float p_78087_4_, float p_78087_5_, float p_78087_6_, Entity p_78087_7_) {
+		this.delegates.forEach(delegate -> delegate.preSetRotationAngles(p_78087_1_, p_78087_2_, p_78087_3_, p_78087_4_, p_78087_5_, p_78087_6_, p_78087_7_));
+		super.setRotationAngles(p_78087_1_, p_78087_2_, p_78087_3_, p_78087_4_, p_78087_5_, p_78087_6_, p_78087_7_);
+		this.delegates.forEach(delegate -> delegate.postSetRotationAngles(p_78087_1_, p_78087_2_, p_78087_3_, p_78087_4_, p_78087_5_, p_78087_6_, p_78087_7_));
+	}
+
+	@Override
+	public void renderEars(float p_78110_1_) {
+		this.delegates.forEach(delegate -> delegate.preRenderEars(p_78110_1_));
+		super.renderEars(p_78110_1_);
+		this.delegates.forEach(delegate -> delegate.postRenderEars(p_78110_1_));
+	}
+
+	@Override
+	public void renderCloak(float p_78111_1_) {
+		this.delegates.forEach(delegate -> delegate.preRenderCloak(p_78111_1_));
+		super.renderCloak(p_78111_1_);
+		this.delegates.forEach(delegate -> delegate.postRenderCloak(p_78111_1_));
 	}
 }
