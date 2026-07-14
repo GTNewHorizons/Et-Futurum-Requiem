e8f4c645e1f5cbc2de088f2458d79ed43361e7a6
Moses Yu <mosesyu1028@gmail.com>
2026-07-14 23:50:33 +0800

    waxing sign fixes

--- src/main/java/ganymedes01/etfuturum/ducks/IWaxableSign.java
+++ src/main/java/ganymedes01/etfuturum/ducks/IWaxableSign.java
@@ -1,7 +1,10 @@
 package ganymedes01.etfuturum.ducks;
 
+import ganymedes01.etfuturum.blocks.BlockWoodSign;
 import ganymedes01.etfuturum.client.particle.CustomParticles;
 import ganymedes01.etfuturum.lib.Reference;
+import net.minecraft.block.Block;
+import net.minecraft.init.Blocks;
 import net.minecraft.world.World;
 
 import java.util.Random;
@@ -23,24 +26,38 @@ public interface IWaxableSign {
 		if (world.isRemote) {
 			playWaxOnSound(world, x, y, z);
 			Random random = world.rand;
-			int meta = world.getBlockMetadata(x, y, z);
-			for (int i = 0; i < 10; ++i) {
+
+			Block block = world.getBlock(x, y, z);
+            boolean isWallSign;
+			if (block instanceof BlockWoodSign) {
+                isWallSign = !((BlockWoodSign) block).standing;
+            }
+            else {
+                isWallSign = block == Blocks.wall_sign;
+            }
+            int totalParticles = isWallSign ? 5 : 10;
+			for (int i = 0; i < totalParticles; ++i) {
 				double px = x + random.nextFloat();
 				double py = y + random.nextFloat();
 				double pz = z + random.nextFloat();
-
-                // Wall sign, move particles to the side of the sign
-				if (meta >= 2 && meta <= 5) {
+                // Offset so particles spawn on the face of the sign
+				if (isWallSign) {
 					double randWallDistance = random.nextFloat() * 0.125D;
-					switch (meta) {
+					switch (world.getBlockMetadata(x, y, z)) {
 						case 2: pz = z + 1.0D - randWallDistance; break;  // south
 						case 3: pz = z + randWallDistance; break;         // north
 						case 4: px = x + 1.0D - randWallDistance; break;  // east
 						case 5: px = x + randWallDistance; break;         // west
 					}
 				}
+                else {
+                    py = y + 0.5D + random.nextFloat() / 2D;
+                }
+
 				CustomParticles.spawnCopperWaxOnParticle(world, px, py, pz);
 			}
 		}
 	}
 }
+
+ 
