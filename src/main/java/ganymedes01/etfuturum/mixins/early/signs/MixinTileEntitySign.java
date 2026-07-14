03eef4245d248534dfcf839ae30599c2a3617ae0
Moses Yu <mosesyu1028@gmail.com>
2026-07-14 18:07:14 +0800

    surpress warning on edit sign

--- src/main/java/ganymedes01/etfuturum/mixins/early/signs/MixinTileEntitySign.java
+++ src/main/java/ganymedes01/etfuturum/mixins/early/signs/MixinTileEntitySign.java
@@ -8,6 +8,7 @@ import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(TileEntitySign.class)
 public abstract class MixinTileEntitySign implements IWaxableSign {
@@ -33,5 +34,16 @@ public abstract class MixinTileEntitySign implements IWaxableSign {
 	@Inject(method = "readFromNBT", at = @At("TAIL"))
 	private void onReadFromNBT(NBTTagCompound nbt, CallbackInfo ci) {
 		waxed = nbt.getBoolean("Waxed");
+	}
+
+	/**
+	 * Vanilla's NetHandlerPlayServer.processUpdateSign checks func_145914_a()
+	 * (is an editing player set?) and logs a warning if not found.
+	 * MixinBlockSign messes with that, so always return true here.
+     * Probably OK to surpress warning? It didn't stop anything before anyways.
+	 */
+	@Inject(method = "func_145914_a", at = @At("HEAD"), cancellable = true)
+	private void onGetIsEditable(CallbackInfoReturnable<Boolean> cir) {
+		cir.setReturnValue(true);
 	}
 }
