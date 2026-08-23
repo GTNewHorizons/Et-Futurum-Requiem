a9264e9760c231fe0c15250b26701233040feda0
hinyb <40139991+hinyb@users.noreply.github.com>
2026-08-23 20:08:51 +0800

    fix: add more vailla hardcoded height patches

--- src/main/java/ganymedes01/etfuturum/asm/pose/HardcodedPlayerHeightTransformer.java
+++ src/main/java/ganymedes01/etfuturum/asm/pose/HardcodedPlayerHeightTransformer.java
@@ -1,5 +1,6 @@
 package ganymedes01.etfuturum.asm.pose;
 
+import cpw.mods.fml.common.asm.transformers.deobf.FMLDeobfuscatingRemapper;
 import ganymedes01.etfuturum.core.utils.Logger;
 import net.minecraft.launchwrapper.IClassTransformer;
 import org.spongepowered.asm.lib.ClassReader;
@@ -27,39 +28,46 @@ public class HardcodedPlayerHeightTransformer implements IClassTransformer {
         targets.put(className, new HashSet<>(Arrays.asList(methods)));
     }
     static {
+        // minecraft
+        addTarget("net.minecraft.block.BlockPistonBase", "determineOrientation(Lnet/minecraft/world/World;IIILnet/minecraft/entity/EntityLivingBase;)I");
+        addTarget("net.minecraft.entity.projectile.EntityFishHook", "<init>(Lnet/minecraft/world/World;Lnet/minecraft/entity/player/EntityPlayer;)V");
+        addTarget("net.minecraft.item.ItemBoat", "func_77659_a(Lnet/minecraft/item/ItemStack;Lnet/minecraft/world/World;Lnet/minecraft/entity/player/EntityPlayer;)Lnet/minecraft/item/ItemStack;", "onItemRightClick(Lnet/minecraft/item/ItemStack;Lnet/minecraft/world/World;Lnet/minecraft/entity/player/EntityPlayer;)Lnet/minecraft/item/ItemStack;");
+        addTarget("net.minecraft.item.ItemEnderEye", "func_77659_a(Lnet/minecraft/item/ItemStack;Lnet/minecraft/world/World;Lnet/minecraft/entity/player/EntityPlayer;)Lnet/minecraft/item/ItemStack;", "onItemRightClick(Lnet/minecraft/item/ItemStack;Lnet/minecraft/world/World;Lnet/minecraft/entity/player/EntityPlayer;)Lnet/minecraft/item/ItemStack;");
+
         // EntityPlayer
-        addTarget("com.darkona.adventurebackpack.item.ItemComponent", "placeBoat");
-        addTarget("appeng.items.tools.powered.ToolMassCannon", "func_77659_a", "onItemRightClick");
-        addTarget("appeng.util.Platform", "rayTrace");
-        addTarget("tuhljin.automagy.lib.TjUtil", "getBlockCoordPlayerIsLookingAt");
-        addTarget("com.arc.bloodarsenal.common.items.sigil.SigilEnder", "func_77659_a", "onItemRightClick");
-        addTarget("com.arc.bloodarsenal.common.items.sigil.SigilLightning", "func_77659_a", "onItemRightClick");
-        addTarget("WayofTime.alchemicalWizardry.common.items.sigil.SigilFluid", "emptyItemToWorld");
-        addTarget("team.chisel.utils.General", "getMovingObjectPositionFromPlayer");
-        addTarget("codechicken.core.commands.PlayerCommand", "getPlayerLookingAtBlock", "getPlayerLookingAtEntity");
-        addTarget("emt.item.tool.ItemThorHammer", "func_77659_a", "onItemRightClick");
-        addTarget("emt.item.ItemMaterials", "onUsingTick");
-        addTarget("fox.spiteful.forbidden.items.wands.ItemFocusBlink", "onFocusRightClick");
-        addTarget("micdoodle8.mods.galacticraft.core.items.ItemOilExtractor", "getNearestOilBlock");
-        addTarget("micdoodle8.mods.galacticraft.core.items.ItemBuggy", "func_77659_a", "onItemRightClick");
-        addTarget("gravisuite.redpower.coreLib", "retraceBlock");
-        addTarget("mrtjp.projectred.expansion.TileMachine", "calcFacing");
-        addTarget("tconstruct.smeltery.items.FilledBucket", "func_77659_a", "onItemRightClick");
-        addTarget("com.kentington.thaumichorizons.common.items.ItemBoatThaumium", "func_77659_a", "onItemRightClick");
-        addTarget("com.kentington.thaumichorizons.common.items.ItemBoatGreatwood", "func_77659_a", "onItemRightClick");
-        addTarget("com.emoniph.witchery.item.ItemGeneral", "placeBroom");
+        addTarget("com.darkona.adventurebackpack.item.ItemComponent", "placeBoat(Lnet/minecraft/item/ItemStack;Lnet/minecraft/world/World;Lnet/minecraft/entity/player/EntityPlayer;Z)Lnet/minecraft/item/ItemStack;");
+        addTarget("appeng.items.tools.powered.ToolMassCannon", "func_77659_a(Lnet/minecraft/item/ItemStack;Lnet/minecraft/world/World;Lnet/minecraft/entity/player/EntityPlayer;)Lnet/minecraft/item/ItemStack;", "onItemRightClick(Lnet/minecraft/item/ItemStack;Lnet/minecraft/world/World;Lnet/minecraft/entity/player/EntityPlayer;)Lnet/minecraft/item/ItemStack;");
+        addTarget("appeng.util.Platform", "rayTrace(Lnet/minecraft/entity/player/EntityPlayer;ZZ)Lnet/minecraft/util/MovingObjectPosition;");
+        addTarget("tuhljin.automagy.lib.TjUtil", "getBlockCoordPlayerIsLookingAt(Lnet/minecraft/entity/player/EntityPlayer;Lnet/minecraft/world/World;)Ltuhljin/automagy/codechicken/lib/vec/BlockCoord;");
+        addTarget("com.arc.bloodarsenal.common.items.sigil.SigilEnder", "func_77659_a(Lnet/minecraft/item/ItemStack;Lnet/minecraft/world/World;Lnet/minecraft/entity/player/EntityPlayer;)Lnet/minecraft/item/ItemStack;", "onItemRightClick(Lnet/minecraft/item/ItemStack;Lnet/minecraft/world/World;Lnet/minecraft/entity/player/EntityPlayer;)Lnet/minecraft/item/ItemStack;");
+        addTarget("com.arc.bloodarsenal.common.items.sigil.SigilLightning", "func_77659_a(Lnet/minecraft/item/ItemStack;Lnet/minecraft/world/World;Lnet/minecraft/entity/player/EntityPlayer;)Lnet/minecraft/item/ItemStack;", "onItemRightClick(Lnet/minecraft/item/ItemStack;Lnet/minecraft/world/World;Lnet/minecraft/entity/player/EntityPlayer;)Lnet/minecraft/item/ItemStack;");
+        addTarget("WayofTime.alchemicalWizardry.common.items.sigil.SigilFluid", "emptyItemToWorld(Lnet/minecraft/item/ItemStack;Lnet/minecraft/world/World;Lnet/minecraft/entity/player/EntityPlayer;)Lnet/minecraft/item/ItemStack;");
+        addTarget("team.chisel.utils.General", "getMovingObjectPositionFromPlayer(Lnet/minecraft/world/World;Lnet/minecraft/entity/player/EntityPlayer;Z)Lnet/minecraft/util/MovingObjectPosition;");
+        addTarget("codechicken.core.commands.PlayerCommand", "getPlayerLookingAtBlock(Lnet/minecraft/entity/player/EntityPlayerMP;F)Lnet/minecraft/world/ChunkPosition;", "getPlayerLookingAtEntity(Lnet/minecraft/entity/player/EntityPlayerMP;F)Lnet/minecraft/entity/Entity;");
+        addTarget("emt.item.tool.ItemThorHammer", "func_77659_a(Lnet/minecraft/item/ItemStack;Lnet/minecraft/world/World;Lnet/minecraft/entity/player/EntityPlayer;)Lnet/minecraft/item/ItemStack;", "onItemRightClick(Lnet/minecraft/item/ItemStack;Lnet/minecraft/world/World;Lnet/minecraft/entity/player/EntityPlayer;)Lnet/minecraft/item/ItemStack;");
+        addTarget("emt.item.ItemMaterials", "onUsingTick(Lnet/minecraft/item/ItemStack;Lnet/minecraft/entity/player/EntityPlayer;I)V");
+        addTarget("fox.spiteful.forbidden.items.wands.ItemFocusBlink", "onFocusRightClick(Lnet/minecraft/item/ItemStack;Lnet/minecraft/world/World;Lnet/minecraft/entity/player/EntityPlayer;Lnet/minecraft/util/MovingObjectPosition;)Lnet/minecraft/item/ItemStack;");
+        addTarget("micdoodle8.mods.galacticraft.core.items.ItemOilExtractor", "getNearestOilBlock(Lnet/minecraft/entity/player/EntityPlayer;)Lmicdoodle8/mods/galacticraft/api/vector/Vector3;");
+        addTarget("micdoodle8.mods.galacticraft.core.items.ItemBuggy", "func_77659_a(Lnet/minecraft/item/ItemStack;Lnet/minecraft/world/World;Lnet/minecraft/entity/player/EntityPlayer;)Lnet/minecraft/item/ItemStack;", "onItemRightClick(Lnet/minecraft/item/ItemStack;Lnet/minecraft/world/World;Lnet/minecraft/entity/player/EntityPlayer;)Lnet/minecraft/item/ItemStack;");
+        addTarget("gravisuite.redpower.coreLib", "retraceBlock(Lnet/minecraft/world/World;Lnet/minecraft/entity/player/EntityPlayer;III)Lnet/minecraft/util/MovingObjectPosition;");
+        addTarget("mrtjp.projectred.expansion.TileMachine", "calcFacing(Lnet/minecraft/entity/player/EntityPlayer;)I");
+        addTarget("tconstruct.smeltery.items.FilledBucket", "func_77659_a(Lnet/minecraft/item/ItemStack;Lnet/minecraft/world/World;Lnet/minecraft/entity/player/EntityPlayer;)Lnet/minecraft/item/ItemStack;", "onItemRightClick(Lnet/minecraft/item/ItemStack;Lnet/minecraft/world/World;Lnet/minecraft/entity/player/EntityPlayer;)Lnet/minecraft/item/ItemStack;");
+        addTarget("com.kentington.thaumichorizons.common.items.ItemBoatThaumium", "func_77659_a(Lnet/minecraft/item/ItemStack;Lnet/minecraft/world/World;Lnet/minecraft/entity/player/EntityPlayer;)Lnet/minecraft/item/ItemStack;", "onItemRightClick(Lnet/minecraft/item/ItemStack;Lnet/minecraft/world/World;Lnet/minecraft/entity/player/EntityPlayer;)Lnet/minecraft/item/ItemStack;");
+        addTarget("com.kentington.thaumichorizons.common.items.ItemBoatGreatwood", "func_77659_a(Lnet/minecraft/item/ItemStack;Lnet/minecraft/world/World;Lnet/minecraft/entity/player/EntityPlayer;)Lnet/minecraft/item/ItemStack;", "onItemRightClick(Lnet/minecraft/item/ItemStack;Lnet/minecraft/world/World;Lnet/minecraft/entity/player/EntityPlayer;)Lnet/minecraft/item/ItemStack;");
+        addTarget("com.emoniph.witchery.item.ItemGeneral", "placeBroom(Lnet/minecraft/item/ItemStack;Lnet/minecraft/entity/player/EntityPlayer;Lnet/minecraft/world/World;IIIIFFF)Z");
+
         // Entity
-        addTarget("pl.asie.lib.block.BlockBase", "determineRotation");
-        addTarget("com.brandon3055.draconicevolution.common.blocks.machine.FlowGate", "determineOrientation");
-        addTarget("com.brandon3055.draconicevolution.common.utils.Utils", "determineOrientation");
-        addTarget("com.enderio.core.client.render.RenderUtil", "createBillboardMatrix");
-        addTarget("micdoodle8.mods.galacticraft.core.proxy.ClientProxyCore", "orientCamera"); // fsub
-        addTarget("mods.natura.blocks.tech.NetherPistonBase", "determineOrientation");
-        addTarget("openblocks.common.entity.EntityGoldenEye", "targetStructure");
-        addTarget("mods.railcraft.common.util.misc.MiscTools", "getSideClosestToPlayer");
-        addTarget("thaumcraft.common.lib.utils.BlockUtils", "getTargetBlock");
-        addTarget("tb.common.block.BlockRelocator", "determineOrientation");
-        addTarget("twilightforest.block.BlockTFNagastoneEtched", "determineOrientation");
+        addTarget("pl.asie.lib.block.BlockBase", "determineRotation(Lnet/minecraft/world/World;IIILnet/minecraft/entity/EntityLivingBase;Lnet/minecraft/item/ItemStack;)I");
+        addTarget("com.brandon3055.draconicevolution.common.blocks.machine.FlowGate", "determineOrientation(Lnet/minecraft/world/World;IIILnet/minecraft/entity/EntityLivingBase;)I");
+        addTarget("com.brandon3055.draconicevolution.common.utils.Utils", "determineOrientation(IIILnet/minecraft/entity/EntityLivingBase;)I");
+        addTarget("com.enderio.core.client.render.RenderUtil", "createBillboardMatrix(Lcom/enderio/core/common/vecmath/Vector3d;Lnet/minecraft/entity/EntityLivingBase;)Lcom/enderio/core/common/vecmath/Matrix4d;");
+        addTarget("micdoodle8.mods.galacticraft.core.proxy.ClientProxyCore", "orientCamera(F)V"); // fsub
+        addTarget("mods.natura.blocks.tech.NetherPistonBase", "determineOrientation(Lnet/minecraft/world/World;IIILnet/minecraft/entity/EntityLivingBase;)I");
+        addTarget("openblocks.common.entity.EntityGoldenEye", "targetStructure(Lnet/minecraft/entity/Entity;Lnet/minecraft/world/ChunkPosition;)V");
+        addTarget("mods.railcraft.common.util.misc.MiscTools", "getSideClosestToPlayer(Lnet/minecraft/world/World;IIILnet/minecraft/entity/EntityLivingBase;)Lnet/minecraftforge/common/util/ForgeDirection;");
+        addTarget("thaumcraft.common.lib.utils.BlockUtils", "getTargetBlock(Lnet/minecraft/world/World;Lnet/minecraft/entity/Entity;Z)Lnet/minecraft/util/MovingObjectPosition;");
+        addTarget("tb.common.block.BlockRelocator", "determineOrientation(Lnet/minecraft/world/World;IIILnet/minecraft/entity/EntityLivingBase;)I");
+        addTarget("twilightforest.block.BlockTFNagastoneEtched", "determineOrientation(Lnet/minecraft/world/World;IIILnet/minecraft/entity/EntityLivingBase;)I");
     }
 
     @Override
@@ -73,38 +81,35 @@ public class HardcodedPlayerHeightTransformer implements IClassTransformer {
         ClassNode cn = new ClassNode();
         cr.accept(cn, ClassReader.SKIP_FRAMES);
 
-        HashSet<String> successfulMethods = new HashSet<>();
-        List<MethodNode> attemptedMethods = new ArrayList<>();
         for (MethodNode method : cn.methods)
         {
-            if (!patchedMethods.contains(method.name)) continue;
-            attemptedMethods.add(method);
+            String mappedMethodName = FMLDeobfuscatingRemapper.INSTANCE.mapMethodName(name, method.name, method.desc);
+            String mappedMethodDesc = FMLDeobfuscatingRemapper.INSTANCE.mapMethodDesc(method.desc);
+            if (!patchedMethods.contains(mappedMethodName + mappedMethodDesc)) continue;
             boolean methodPatched = false;
             for (AbstractInsnNode instruction = method.instructions.getFirst(); instruction != null; instruction = instruction.getNext())
             {
                 if (instruction.getOpcode() == Opcodes.GETFIELD)
                 {
                     FieldInsnNode fin = (FieldInsnNode) instruction;
-                    if (("yOffset".equals(fin.name) || "field_70129_M".equals(fin.name)) && "F".equals(fin.desc) && isTarget(fin)) {
-                        methodPatched = true;
-                        method.instructions.insertBefore(instruction, new InsnNode(Opcodes.DUP));
-                        MethodInsnNode hook = new MethodInsnNode(Opcodes.INVOKESTATIC, "ganymedes01/etfuturum/asm/pose/HardcodedPlayerHeightHook", "adjustHeight", "(Lnet/minecraft/entity/Entity;F)F", false);
-                        method.instructions.insert(instruction, hook);
+                    if ("F".equals(fin.desc)) {
+                        String fieldName = FMLDeobfuscatingRemapper.INSTANCE.mapFieldName(fin.owner, fin.name, fin.desc);
+                        if (("yOffset".equals(fieldName) || "field_70129_M".equals(fieldName)) && isTarget(fin)) {
+                            methodPatched = true;
+                            method.instructions.insertBefore(instruction, new InsnNode(Opcodes.DUP));
+                            MethodInsnNode hook = new MethodInsnNode(Opcodes.INVOKESTATIC, "ganymedes01/etfuturum/asm/pose/HardcodedPlayerHeightHook", "adjustHeight", "(Lnet/minecraft/entity/Entity;F)F", false);
+                            method.instructions.insert(instruction, hook);
+                        }
                     }
                 }
             }
-            if (methodPatched){
-                successfulMethods.add(method.name);
+            if (!methodPatched){
+                Logger.warn("Failed to patch hardcoded height in " + transformedName +"."+ method.name + method.desc + ": target bytecode not found.");
+            } else {
+                Logger.info("succeed to patch" + transformedName);
             }
         }
 
-        for (MethodNode method : attemptedMethods)
-        {
-            if (!successfulMethods.contains(method.name))
-            {
-                Logger.warn("Failed to patch hardcoded height in " + transformedName +"."+ method.name + method.desc + ": target bytecode not found.");
-            }
-        }
         MixinClassWriter cw = new MixinClassWriter(MixinClassWriter.COMPUTE_MAXS | MixinClassWriter.COMPUTE_FRAMES);
         cn.accept(cw);
         return cw.toByteArray();
