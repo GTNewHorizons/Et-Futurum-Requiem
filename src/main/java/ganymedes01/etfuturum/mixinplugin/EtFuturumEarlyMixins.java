79e0d2885d13f2aed35489101d7cec9bc83120d7
Nana Sakura <nana-sakura@qq.com>
2026-09-15 01:17:08 +0800

    backport modern spawning behavior

--- src/main/java/ganymedes01/etfuturum/mixinplugin/EtFuturumEarlyMixins.java
+++ src/main/java/ganymedes01/etfuturum/mixinplugin/EtFuturumEarlyMixins.java
@@ -340,6 +340,10 @@ public class EtFuturumEarlyMixins implements IFMLLoadingPlugin, IEarlyMixinLoade
 			mixins.add("foxes.MixinEntityWolf");
 		}
 
+        if (ConfigEntities.enableModernWither) {
+            mixins.add("wither.MixinBlockSkull_OnPlacingWitherSkull");
+        }
+
 		return mixins;
 	}
 
